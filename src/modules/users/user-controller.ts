b26be243492fe6d69964195d912af23cd683a6c3
nizaelvalente <nizaelvalente@gmail.com>
2022-04-27 03:15:02 -0300

    update na rota deleteMultiple

--- src/modules/users/user-controller.ts
+++ src/modules/users/user-controller.ts
@@ -49,9 +49,10 @@ export = {
     },
     async deleteMultiple (req:Request) {
         try {
-            const response =  db
-            if(!response) return {status: 408, data:{message:"Não ha usuário para apagar."}}
-            return {status: 200, data:response}
+            const response  = await req.body.map((id:Number) => db.filter((e:any) => e.id === id))
+
+            if(!response[0].length) return {status: 408, data:{message:"Não ha usuário para apagar."}}
+            return {status: 200, data:response[0]}
         } catch (error) {
             return {status: 400, data:{message:error}}
         }
