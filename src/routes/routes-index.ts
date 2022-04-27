22934140214253ac00d97c8010328b3cc7d83cc1
nizaelvalente <nizaelvalente@gmail.com>
2022-04-27 00:57:36 -0300

    inicio do endpoint  de produtos

--- src/routes/routes-index.ts
+++ src/routes/routes-index.ts
@@ -6,6 +6,9 @@ const routes = Router()
 // routes.use('/', routersUsers)
 
 routes.use('/user', routersUsers)
+routes.use('/product', (req, res)=> {
+    return res.status(200).send('products')
+})
 
 
 export default routes
